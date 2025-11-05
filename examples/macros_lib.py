6a250622722e9c5b22a3c26eed8aad77d3893463
CoolMage <savva.arutsev@gmail.com>
2025-11-05 10:14:48 +0100

    Refactor macro definitions and enhance image processing steps
    - Renamed macros for clarity, including "mip_rois_measure_for_channel" to "mip_roi_measure_AllChannels".
    - Updated measurement settings in the image processing workflow to include median and area fraction.
    - Adjusted threshold values for particle analysis and refined size and circularity parameters.
    - Added a new macro "AllImgae_AP_OneChannle2_CLAHE_WithRoi" for enhanced local contrast and ROI management.

--- examples/macros_lib.py
+++ examples/macros_lib.py
@@ -66,7 +66,7 @@ run("Quit");
 ''',
 
 
-    "mip_rois_measure_for_channel" : '''
+    "mip_roi_measure_AllChannels" : '''
 // --- Open & prepare (Bio-Formats + Z-MIP) ---
 setBatchMode(true);
 run("Bio-Formats Importer", "open=[{img_path_fiji}] autoscale color_mode=Default rois_import=[ROI manager] view=Hyperstack stack_order=XYCZT");
@@ -74,7 +74,6 @@ origTitle = getTitle();
 run("Z Project...", "projection=[Max Intensity]");
 selectWindow(origTitle); run("Close");
 rename("{file_stem}_MIP");
-saveAs("Tiff", "{output_dir_native}/{file_stem}_MIP");
 
 // --- Load ROIs once ---
 roiManager("Reset");
@@ -86,13 +85,18 @@ if (File.exists(roiZip)) {{
     roiManager("Open", roiRoi);
 }} else {{
     print("WARN: ROI file not found: " + roiZip + " or " + roiRoi);
+    run("Close All");
+    run("Quit");
 }}
 run("Select None");
+
 
 // --- Split and loop channels ---
 run("Split Channels");
 img_list = getList("image.titles");
-run("Set Measurements...", "area mean min max std integrated redirect=None decimal=3");
+
+// --- Set Measurements ---
+run("Set Measurements...", "area mean standard min integrated median area_fraction redirect=None decimal=3");
 
 for (i = 0; i < img_list.length; i++) {{
     selectWindow(img_list[i]);
@@ -166,12 +170,12 @@ run("Quit");
 
 ''',
 
-    "all_image_analise_particule_one_channle_RECA" : '''
+    "all_image_analise_particule_one_channle" : '''
 // --- Open & keep only Ch1 + Z1 via duplication ---
 run("Bio-Formats Importer", "open=[{img_path_fiji}] autoscale color_mode=Default rois_import=[ROI manager] view=Hyperstack stack_order=XYCZT");
 origTitle = getTitle();
 
-// дублируем строго 1-й канал, 1-й Z-срез, 1-й кадр
+// дублируем строго 2-й канал, 2-й Z-срез, 2-й кадр
 dupTitle = "{document_name}_ch2z2";
 run("Duplicate...", "title=["+dupTitle+"] duplicate channels=2 slices=2");
 selectWindow(dupTitle);
@@ -183,24 +187,93 @@ selectWindow(dupTitle);
 
 // --- Threshold ---
 getRawStatistics(nPixels, mean, minVal, maxVal);
-minThr = 400;
+minThr = 1350;
 if (maxVal < minThr) minThr = maxVal; // на случай 8-bit/аномалий
 setThreshold(minThr, maxVal);
 setOption("BlackBackground", false);
 run("Convert to Mask");  // делаем бинарную маску из порога
 
-saveAs("Tiff", "{output_dir_native}/{document_name}_ch2z2_thr400");
+saveAs("Tiff", "{output_dir_native}/{document_name}_ch2z2_thr1350");
 
 // --- Analyze Particles + сохранить ROIs ---
 run("Set Measurements...", "area mean min max std integrated centroid feret's redirect=None decimal=3");
 roiManager("Reset");
-run("Analyze Particles...", "size=15-750 circularity=0.50-1.00 clear include add");
+run("Analyze Particles...", "size=25-1200 circularity=0.30-1.00 clear include add");
 
 // --- Save results table to CSV ---
 saveAs("Results", "{out_csv}");
 
 // --- Save ROIs as ZIP (имя документа + пометка) ---
 roiZip = "{output_dir_native}/{document_name}_ch2z2_thr400_part15-750_ROIs.zip";
+roiManager("Save", roiZip);
+
+// --- Cleanup ---
+run("Close All");
+run("Quit");
+
+''',
+
+    "AllImgae_AP_OneChannle2_CLAHE_WithRoi" : '''
+// --- Open & keep only Ch1 + Z1 via duplication ---
+run("Bio-Formats Importer", "open=[{img_path_fiji}] autoscale color_mode=Default rois_import=[ROI manager] view=Hyperstack stack_order=XYCZT");
+origTitle = getTitle();
+
+// дублируем строго 2-й канал, 2-й Z-срез, 2-й кадр
+dupTitle = "{document_name}_ch2z2";
+run("Duplicate...", "title=["+dupTitle+"] duplicate channels=2 slices=2");
+selectWindow(dupTitle);
+
+// (опционально) закрыть исходник, чтобы не мешал
+selectWindow(origTitle);
+close();
+selectWindow(dupTitle);
+
+// --- Make a MIP ---
+origTitle = getTitle();
+run("Z Project...", "projection=[Max Intensity]");
+selectWindow(origTitle); run("Close");
+rename("{file_stem}_MIP");
+
+// --- Enhance Local Contrast (CLAHE) ---
+
+run("Enhance Local Contrast (CLAHE)", "blocksize=127 histogram=256 maximum=3 mask=*None* fast_(less_accurate)");
+
+// --- Threshold ---
+getRawStatistics(nPixels, mean, minVal, maxVal);
+minThr = 1350;
+if (maxVal < minThr) minThr = maxVal; // на случай 8-bit/аномалий
+setThreshold(minThr, maxVal);
+setOption("BlackBackground", false);
+run("Convert to Mask");  // делаем бинарную маску из порога
+
+saveAs("Tiff", "{output_dir_native}/{document_name}_ch2z2_thr1350_CLAHE");
+
+// --- Load ROIs once ---
+roiManager("Reset");
+roiZip = "{img_dir_fiji_slash}{document_name}.zip";
+roiRoi = "{img_dir_fiji_slash}{document_name}.roi";
+if (File.exists(roiZip)) {{
+    roiManager("Open", roiZip);
+}} else if (File.exists(roiRoi)) {{
+    roiManager("Open", roiRoi);
+}} else {{
+    print("WARN: ROI file not found: " + roiZip + " or " + roiRoi);
+    run("Close All");
+    run("Quit");
+}}
+run("Select None");
+
+
+// --- Analyze Particles + сохранить ROIs ---
+run("Set Measurements...", "area mean min max std integrated centroid feret's redirect=None decimal=3");
+roiManager("Reset");
+run("Analyze Particles...", "size=25-1200 circularity=0.30-1.00 clear include add");
+
+// --- Save results table to CSV ---
+saveAs("Results", "{out_csv}");
+
+// --- Save ROIs as ZIP (имя документа + пометка) ---
+roiZip = "{output_dir_native}/{document_name}_ch2z2_thr1350_part25-1200_CLAHE_ROIs.zip";
 roiManager("Save", roiZip);
 
 // --- Cleanup ---
